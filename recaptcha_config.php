68bf8366c8646a94d08ba8720d8a3d7661b43193
hostingersentme <91921343+hostingersentme@users.noreply.github.com>
2025-06-04 17:46:05 +1200

    Create recaptcha_config.php

---
+++ recaptcha_config.php
@@ -0,0 +1,8 @@
+<?php
+// recaptcha_config.php
+
+return [
+    'site_key' => 'Replace with your actual site key',     // Replace with your actual site key
+    'secret_key' => 'Replace with your actual secret key-'  // Replace with your actual secret key
+];
+?>
